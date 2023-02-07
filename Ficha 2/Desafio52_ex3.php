edf207570142d5719ca740978515e675a0bcd8c9
Mosty159 <guifilipe41@gmail.com>
2023-02-07 11:40:48 +0000

    oi

---
+++ Ficha 2/Desafio52_ex3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Desafio 5.2 - ex3</title>
+</head>
+<body>
+
+    <Form action = ".php" method="GET">
+        Primeiro nº: <input type="text" name="a"> <br>
+        Segundo nº: <input type="text" name="b"> <br>
+                    <input type="submit" value = "Calcular">
+    </Form>
+
+    <?php
+
+        $a = $_GET['a'];
+        $b = $_GET['b'];
+
+        echo $a ," + ", $b, " = " ,$a + $b;
+
+    ?>
+</body>
+</html>

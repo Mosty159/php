76d341a692e6c8767d4a1173ac7d885c1974cda4
Mosty159 <guifilipe41@gmail.com>
2023-03-16 23:38:25 +0000

    oi

--- Ficha4/EXE4.php
+++ Ficha4/EXE4.php
@@ -6,52 +6,52 @@
 </head>
 <body>
 
+    <Form action = "<?php echo $_SERVER['PHP_SELF']?>" method="POST">
 
-    <form action="" method = "POST">
+    <?php
+        if(!isset($_POST['Mostrar'])){
 
-        <?php
-            if (!isset($_POST['Mostrar'])) {
+        $dia = 1;
+        $meses = array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+        $ano = 1980;
 
-                $dia = 1;
-                $meses = array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
-                $ano = 1980;
+        echo 'Dia: <select name = "dia">';
+        for($a = 0; $a < 31; $a++){
+            echo '<option value= "' , $dia ,'">' , $dia , '</option>';
+            $dia++;
+        }
+        echo '</select><br><br>';
 
-
-                echo 'Dia: <select name="dia">';
-                for ($a1 = 0; $a1 < 31; $a1++){ 
-                echo '<option value="', $dia, '">', $dia, '</option>';
-                $dia++;
-                }
-                echo '</select><br><br>';
-
-
-                echo 'Mês: <select name="meses">';
-                for ($a2 = 0; $a2 < count($meses); $a2++){
-                echo '<option value="', $meses[$a2], '">', $meses[$a2], '</option>';
-                }
-                echo '</select><br><br>';
+        echo 'Mês: <select name = "meses">';
+        for($a = 0; $a < count($meses); $a++){
+            echo '<option value= "' , $meses[$a] , '">' , $meses[$a] , '</option>';
+        }
+        echo "</select> <br> <br>";
 
 
-                echo 'Ano: <select name="ano">';
-                for ($a3 = 1980; $a3 <= 2023; $a3++){
-                echo '<option value="', $ano, '">', $ano, '</option>';
-                $ano++;
-                }
-                echo '</select><br><br>';
-                echo '<input type="submit" name ="Mostrar" value ="Mostrar">';
-            }
-            else{
+        echo 'Ano: <select name = "ano">';
+        for($a = 1980; $a <= 2023; $a++){
+            echo '<option value= "' , $ano , '">' , $ano , '</option>';
+            $ano++;
+        }
+        echo '</select> <br> <br>';
 
-                $diaa = $_POST['dia'];
-                $mes = $_POST['meses'];
-                $anoo = $_POST['ano'];
-                echo "Foi escolhido o dia $diaa do mês de $mes do ano de $anoo <br><br>";
-                echo '<input type="submit" name ="Voltar" value ="Voltar">';
-            }
-        ?>
+        echo '<input type="submit" name = "Mostrar" value = "Mostrar">';
 
 
-    </form>
+        }
+        else{
 
+            $dai = $_POST['dia'];
+            $mes = $_POST['meses'];
+            $anu = $_POST['ano'];
+
+            echo "Foi escolhido o dia $dai do mês de $mes do ano de $anu <br><br>";
+
+            echo '<input type="submit" name = "Voltar" value="Voltar">';
+        }
+    ?>
+
+    </Form>
 </body>
 </html>
